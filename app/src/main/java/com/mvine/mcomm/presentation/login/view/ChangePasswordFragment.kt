60234c40454b9126250c49bfa96fbf500342e2ab
Dheeraj Andra <sai.sreedheeraj@gmail.com>
2021-12-05 09:02:54 +0530

    Login: Login Flow screens added

--- app/src/main/java/com/mvine/mcomm/presentation/login/view/ChangePasswordFragment.kt
+++ app/src/main/java/com/mvine/mcomm/presentation/login/view/ChangePasswordFragment.kt
@@ -5,7 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import com.mvine.mcomm.R
 
-class LoginFragment: Fragment(R.layout.fragment_login) {
+class ChangePasswordFragment: Fragment(R.layout.fragment_change_password) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/com/mvine/mcomm/presentation/login/view/LoginMenuFragment.kt
+++ app/src/main/java/com/mvine/mcomm/presentation/login/view/LoginMenuFragment.kt
@@ -5,7 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import com.mvine.mcomm.R
 
-class LoginFragment: Fragment(R.layout.fragment_login) {
+class LoginMenuFragment: Fragment(R.layout.fragment_login_menu) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
