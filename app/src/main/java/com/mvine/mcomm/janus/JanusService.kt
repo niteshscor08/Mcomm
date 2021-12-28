86742f235c1ae776ebece77412b8fdfb8309a0fd
shweta kumari <Shweta_kumari@condenast.com>
2021-12-28 23:13:01 +0530

    audio_functionality added janus response

--- app/src/main/java/com/mvine/mcomm/janus/JanusService.kt
+++ app/src/main/java/com/mvine/mcomm/janus/JanusService.kt
@@ -1,16 +1,32 @@
 package com.mvine.mcomm.janus
 
 import android.util.Log
+import com.google.gson.Gson
+import com.mvine.janusclient.*
+import com.mvine.mcomm.data.model.response.PersonInfo
 import com.mvine.mcomm.janus.request.JanusCreate
+import com.mvine.mcomm.janus.request.JanusRegister
 import com.mvine.mcomm.janus.utils.CommonValues.CREATE_KEY_LENGTH
 import com.mvine.mcomm.janus.utils.RandomString
+import com.mvine.mcomm.util.PreferenceHandler
+import com.mvine.mcomm.util.USER_INFO
 import com.tinder.scarlet.Message
 import com.tinder.scarlet.WebSocket
 import io.reactivex.android.schedulers.AndroidSchedulers
+import org.json.JSONObject
+import org.webrtc.MediaStream
+import org.webrtc.PeerConnection
+import javax.inject.Inject
 
 class JanusService(private val webSocketService: WebSocketService) {
 
-     fun connect() {
+    @Inject
+    lateinit var preferenceHandler: PreferenceHandler
+
+    private val janusServer = JanusServer(JanusGlobalCallbacks())
+
+
+    fun connect() {
         webSocketService.observeConnection()
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({ response ->
@@ -33,10 +49,101 @@ class JanusService(private val webSocketService: WebSocketService) {
 
     private fun handleOnMessageReceived(message: Message) {
         Log.i("connection", message.toString())
+        when(message.toString()){
+             JanusMessageType.success.toString() -> {
+                when()
+            }
+        }
     }
 
     fun sendMessage(message: String) {
-        webSocketService.sendMessage(JanusCreate(transaction = RandomString().randomString(CREATE_KEY_LENGTH)))
+        webSocketService.sendMessage(message)
+    }
+
+    fun createJanusSession() {
+        sendMessage(Gson().toJson(JanusCreate()))
+    }
+
+    inner class JanusGlobalCallbacks : IJanusGatewayCallbacks {
+        override fun onCallbackError(error: String?) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onSuccess() {
+            janusServer.Attach(JanusPluginCallbacks())
+        }
+
+        override fun onDestroy() {
+            TODO("Not yet implemented")
+        }
+
+        override fun getServerUri(): String {
+            TODO("Not yet implemented")
+        }
+
+        override fun getIceServers(): MutableList<PeerConnection.IceServer> {
+            TODO("Not yet implemented")
+        }
+
+        override fun getIpv6Support(): Boolean {
+            TODO("Not yet implemented")
+        }
+
+        override fun getMaxPollEvents(): Int {
+            TODO("Not yet implemented")
+        }
+
+    }
+
+    inner class JanusPluginCallbacks : IJanusPluginCallbacks {
+        override fun onCallbackError(error: String?) {
+            TODO("Not yet implemented")
+        }
+
+        override fun success(handle: JanusPluginHandle?) {
+            startSIPRegistration()
+        }
+
+        override fun onMessage(msg: JSONObject?, jsep: JSONObject?) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onLocalStream(stream: MediaStream?) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onRemoteStream(stream: MediaStream?) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onDataOpen(data: Any?) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onData(data: Any?) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onCleanup() {
+            TODO("Not yet implemented")
+        }
+
+        override fun onDetached() {
+            TODO("Not yet implemented")
+        }
+
+        override fun onMediaMessage(type: String?, receiving: Boolean) {
+            TODO("Not yet implemented")
+        }
+
+        override fun getPlugin(): JanusSupportedPluginPackages {
+            return JanusSupportedPluginPackages.JANUS_SIP
+        }
+    }
+
+    fun startSIPRegistration() {
+        val personInfo : PersonInfo = preferenceHandler.get(USER_INFO)
+        
     }
 
 }
--- app/src/main/java/com/mvine/mcomm/janus/request/JanusRequests.kt
+++ app/src/main/java/com/mvine/mcomm/janus/request/JanusRequests.kt
@@ -4,37 +4,37 @@ import android.os.Parcelable
 import com.mvine.janusclient.JanusMessageType
 import com.mvine.janusclient.JanusSupportedPluginPackages
 import com.mvine.janusclient.TransactionType
+import com.mvine.mcomm.janus.utils.CommonValues
+import com.mvine.mcomm.janus.utils.RandomString
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class JanusCreate (
     val janus: String= JanusMessageType.create.toString(),
-    val transaction: String?= null
+    val transaction: String = RandomString().randomString(CommonValues.CREATE_KEY_LENGTH)
 ): Parcelable
 
 @Parcelize
 data class JanusRegister(
-    val janus: String = JanusMessageType.message.toString(),
-    val session_id: Long,
-    val handle_id: Long,
+    //val janus: String = JanusMessageType.message.toString(),
+    //val session_id: Long,
+   // val handle_id: Long,
     val body: Body
 ): Parcelable{
     @Parcelize
     data class Body(
-    val ptype: String = "publisher",
-    var display: String? = null,
     val request: String = "register",
     val proxy: String = "sip:portaluat.mvine.com:5068",
-    var authuser: String? = null,
-    var username: String? = null,
-    var display_name: String? = null,
-    var secret: String? = null
+    var authuser: String,
+    var username: String,
+    var display_name: String,
+    var secret: String
     ): Parcelable
 }
 
 @Parcelize
 data class JanusAttach(
-    val id: Long? = null,
+    val id: Long,
     val transaction: String = TransactionType.attach.toString(),
     val plugin: String = JanusSupportedPluginPackages.JANUS_SIP.toString()
     ): Parcelable
