4c87d43822286366c5de1d247d6185e04b95ebd4
shweta kumari <Shweta_kumari@condenast.com>
2022-01-12 12:34:28 +0530

    login removed unused keys

--- app/src/main/java/com/mvine/mcomm/util/CommonUtils.kt
+++ app/src/main/java/com/mvine/mcomm/util/CommonUtils.kt
@@ -3,7 +3,5 @@ package com.mvine.mcomm.util
 const val MCOMM_SHARED_PREFERENCES = "Mcomm"
 const val LOGIN_TOKEN = "Login Token"
 const val CREDENTIAL_DATA = "Credential Data"
-const val USER_NAME = "User Name"
-const val USER_PASSWORD = "User Password"
 const val USER_INFO = "User Info"
 const val EMPTY_STRING = ""
